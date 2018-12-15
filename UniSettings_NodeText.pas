2fb01ba718d5665dd929ded05f1a606ef4135eb7
František Milt <frantisek.milt@gmail.com>
2018-12-15 14:11:34 +0100

    Reimplemented Time, Text and Buffer nodes

--- UniSettings_NodeText.pas
+++ UniSettings_NodeText.pas
@@ -2,6 +2,7 @@
 unit UniSettings_NodeText;
 
 {$INCLUDE '.\UniSettings_defs.inc'}
+{$DEFINE UNS_NodeText}
 
 interface
 
@@ -11,38 +12,14 @@ uses
   UniSettings_Common, UniSettings_NodeBase, UniSettings_NodeLeaf;
 
 type
+  TUNSNodeValueType    = String;
+  TUNSNodeValueTypeBin = String;
+  TUNSNodeValueTypePtr = PString;
+
   TUNSNodeText = class(TUNSNodeLeaf)
-  private
-    fValue:         String;
-    fSavedValue:    String;
-    fDefaultValue:  String;
-    procedure SetValue(NewValue: String);
-    procedure SetDefaultValue(NewValue: String);
-  protected
-    class Function GetValueType: TUNSValueType; override;
-    Function GetValueSize: TMemSize; override;
-    Function GetDefaultValueSize: TMemSize; override;
-    Function ConvToStr(const Value): String; override;
-    Function ConvFromStr(const Str: String): Pointer; override;
-  public
-    constructor Create(const Name: String; ParentNode: TUNSNodeBase);
-    constructor CreateAsCopy(Source: TUNSNodeBase; const Name: String; ParentNode: TUNSNodeBase);
-    procedure ActualFromDefault; override;
-    procedure DefaultFromActual; override;
-    procedure ExchangeActualAndDefault; override;
-    Function ActualEqualsDefault: Boolean; override;
-    procedure Save; override;
-    procedure Restore; override;
-    Function Address(AccessDefVal: Boolean = False): Pointer; override;
-    Function AsString(AccessDefVal: Boolean = False): String; override;
-    procedure FromString(const Str: String; AccessDefVal: Boolean = False); override;
-    procedure ToStream(Stream: TStream; AccessDefVal: Boolean = False); override;
-    procedure FromStream(Stream: TStream; AccessDefVal: Boolean = False); override;
-    procedure ToBuffer(Buffer: TMemoryBuffer; AccessDefVal: Boolean = False); override;
-    procedure FromBuffer(Buffer: TMemoryBuffer; AccessDefVal: Boolean = False); override;
-    property Value: String read fValue write SetValue;
-    property SavedValue: String read fSavedValue;
-    property DefaultValue: String read fDefaultValue write SetDefaultValue;
+  {$DEFINE UNS_NodeInclude_Declaration}
+    {$INCLUDE '.\UniSettings_Node.inc'}
+  {$UNDEF UNS_NodeInclude_Declaration}
   end;
 
 implementation
@@ -52,64 +29,50 @@ uses
   BinaryStreaming, StrRect,
   UniSettings_Exceptions;
 
-procedure TUNSNodeText.SetValue(NewValue: String);
-begin
-If not AnsiSameStr(NewValue,fValue) then
-  begin
-    fValue := NewValue;
-    DoChange;
-  end;
-end;
+type
+  TUNSNodeClassType = TUNSNodeText;
 
-//------------------------------------------------------------------------------
+var
+  UNS_StreamWriteFunction:
+    Function(Stream: TStream; const Value: String; Advance: Boolean = True): TMemSize
+      = BinaryStreaming.Stream_WriteString;
 
-procedure TUNSNodeText.SetDefaultValue(NewValue: String);
-begin
-If not AnsiSameStr(NewValue,fDefaultValue) then
-  begin
-    fDefaultValue := NewValue;
-    DoChange;
-  end;
-end;
+  UNS_StreamReadFunction:
+    Function(Stream: TStream; Advance: Boolean = True): String
+      = BinaryStreaming.Stream_ReadString;
+
+  UNS_BufferWriteFunction:
+    Function(Dest: Pointer; const Value: String): TMemSize
+      = BinaryStreaming.Ptr_WriteString;
+      
+  UNS_BufferReadFunction:
+    Function(Dest: Pointer): String
+      = BinaryStreaming.Ptr_ReadString;
 
 //==============================================================================
 
-class Function TUNSNodeText.GetValueType: TUNSValueType;
+class Function TUNSNodeClassType.GetValueType: TUNSValueType;
 begin
 Result := vtText;
 end;
 
 //------------------------------------------------------------------------------
 
-Function TUNSNodeText.GetValueSize: TMemSize;
+Function TUNSNodeClassType.ConvToStr(Value: TUNSNodeValueType): String;
 begin
-Result := SizeOf(Int32) + Length(StrToUTF8(fValue)) * SizeOf(UTF8Char);
+Result := Value;
 end;
 
 //------------------------------------------------------------------------------
 
-Function TUNSNodeText.GetDefaultValueSize: TMemSize;
+Function TUNSNodeClassType.ConvFromStr(const Str: String): TUNSNodeValueType;
 begin
-Result := SizeOf(Int32) + Length(StrToUTF8(fDefaultValue)) * SizeOf(UTF8Char);
-end;
-
-//------------------------------------------------------------------------------
-
-Function TUNSNodeText.ConvToStr(const Value): String;
-begin
-Result := '';
-end;
-
-//------------------------------------------------------------------------------
-
-Function TUNSNodeText.ConvFromStr(const Str: String): Pointer;
-begin
-Result := nil;
+Result := Str;
 end;
 
 //==============================================================================
 
-constructor TUNSNodeText.Create(const Name: String; ParentNode: TUNSNodeBase);
+constructor TUNSNodeClassType.Create(const Name: String; ParentNode: TUNSNodeBase);
 begin
 inherited Create(Name,ParentNode);
 fValue := '';
@@ -119,149 +82,9 @@ end;
 
 //------------------------------------------------------------------------------
 
-constructor TUNSNodeText.CreateAsCopy(Source: TUNSNodeBase; const Name: String; ParentNode: TUNSNodeBase);
-begin
-inherited CreateAsCopy(Source,Name,ParentNode);
-fValue := TUNSNodeText(Source).Value;
-fSavedValue := TUNSNodeText(Source).SavedValue;
-fDefaultValue := TUNSNodeText(Source).DefaultValue;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeText.ActualFromDefault;
-begin
-If not ActualEqualsDefault then
-  begin
-    fValue := fDefaultValue;
-    DoChange;
-  end;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeText.DefaultFromActual;
-begin
-If not ActualEqualsDefault then
-  begin
-    fDefaultValue := fValue;
-    DoChange;
-  end;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeText.ExchangeActualAndDefault;
-var
-  Temp: String;
-begin
-If not ActualEqualsDefault then
-  begin
-    Temp := fDefaultValue;
-    fDefaultValue := fValue;
-    fValue := Temp;
-    DoChange;
-  end;
-end;
-
-//------------------------------------------------------------------------------
-
-Function TUNSNodeText.ActualEqualsDefault: Boolean;
-begin
-Result := AnsiSameStr(fValue,fDefaultValue);
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeText.Save;
-begin
-fSavedValue := fValue;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeText.Restore;
-begin
-SetValue(fSavedValue);
-end;
-
-//------------------------------------------------------------------------------
-
-Function TUNSNodeText.Address(AccessDefVal: Boolean = False): Pointer;
-begin
-If AccessDefVal then
-  Result := PChar(fDefaultValue)
-else
-  Result := PChar(fValue);
-end;
-
-//------------------------------------------------------------------------------
-
-Function TUNSNodeText.AsString(AccessDefVal: Boolean = False): String;
-begin
-If AccessDefVal then
-  Result := fDefaultValue
-else
-  Result := fValue;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeText.FromString(const Str: String; AccessDefVal: Boolean = False);
-begin
-If AccessDefVal then
-  SetDefaultValue(Str)
-else
-  SetValue(Str);
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeText.ToStream(Stream: TStream; AccessDefVal: Boolean = False);
-begin
-If AccessDefVal then
-  Stream_WriteString(Stream,fDefaultValue)
-else
-  Stream_WriteString(Stream,fValue);
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeText.FromStream(Stream: TStream; AccessDefVal: Boolean = False);
-begin
-If AccessDefVal then
-  SetDefaultValue(Stream_ReadString(Stream))
-else
-  SetValue(Stream_ReadString(Stream));
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeText.ToBuffer(Buffer: TMemoryBuffer; AccessDefVal: Boolean = False);
-begin
-If Buffer.Size >= ObtainValueSize(AccessDefVal) then
-  begin
-    If AccessDefVal then
-      Ptr_WriteString(Buffer.Memory,fDefaultValue)
-    else
-      Ptr_WriteString(Buffer.Memory,fValue);
-  end
-else raise EUNSBufferTooSmallException.Create(Buffer,Self,'GetValueToBuffer');
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeText.FromBuffer(Buffer: TMemoryBuffer; AccessDefVal: Boolean = False);
-begin
-If Buffer.Size >= SizeOf(Int32) then
-  begin
-    If AccessDefVal then
-      SetDefaultValue(Ptr_ReadString(Buffer.Memory))
-    else
-      SetValue(Ptr_ReadString(Buffer.Memory));
-  end
-else raise EUNSBufferTooSmallException.Create(Buffer,Self,'SetValueFromBuffer');
-end;
+{$DEFINE UNS_NodeInclude_Implementation}
+  {$INCLUDE '.\UniSettings_Node.inc'}
+{$UNDEF UNS_NodeInclude_Implementation}
 
 {$WARNINGS OFF} // supresses warnings on lines after the final end
 end.
--- UniSettings_NodeTime.pas
+++ UniSettings_NodeTime.pas
@@ -2,6 +2,7 @@
 unit UniSettings_NodeTime;
 
 {$INCLUDE '.\UniSettings_defs.inc'}
+{$DEFINE UNS_NodeTime}
 
 interface
 
@@ -11,38 +12,14 @@ uses
   UniSettings_Common, UniSettings_NodeBase, UniSettings_NodeLeaf;
 
 type
+  TUNSNodeValueType    = TTime;
+  TUNSNodeValueTypeBin = TTime;
+  TUNSNodeValueTypePtr = PTime;
+
   TUNSNodeTime = class(TUNSNodeLeaf)
-  private
-    fValue:         TTime;
-    fSavedValue:    TTime;
-    fDefaultValue:  TTime;
-    procedure SetValue(NewValue: TTime);
-    procedure SetDefaultValue(NewValue: TTime);
-  protected
-    class Function GetValueType: TUNSValueType; override;
-    Function GetValueSize: TMemSize; override;
-    Function GetDefaultValueSize: TMemSize; override;
-    Function ConvToStr(Value: TTime): String; reintroduce;
-    Function ConvFromStr(const Str: String): TTime; reintroduce;
-  public
-    constructor Create(const Name: String; ParentNode: TUNSNodeBase);
-    constructor CreateAsCopy(Source: TUNSNodeBase; const Name: String; ParentNode: TUNSNodeBase);
-    procedure ActualFromDefault; override;
-    procedure DefaultFromActual; override;
-    procedure ExchangeActualAndDefault; override;
-    Function ActualEqualsDefault: Boolean; override;
-    procedure Save; override;
-    procedure Restore; override;
-    Function Address(AccessDefVal: Boolean = False): Pointer; override;
-    Function AsString(AccessDefVal: Boolean = False): String; override;
-    procedure FromString(const Str: String; AccessDefVal: Boolean = False); override;
-    procedure ToStream(Stream: TStream; AccessDefVal: Boolean = False); override;
-    procedure FromStream(Stream: TStream; AccessDefVal: Boolean = False); override;
-    procedure ToBuffer(Buffer: TMemoryBuffer; AccessDefVal: Boolean = False); override;
-    procedure FromBuffer(Buffer: TMemoryBuffer; AccessDefVal: Boolean = False); override;
-    property Value: TTime read fValue write SetValue;
-    property SavedValue: TTime read fSavedValue;
-    property DefaultValue: TTime read fDefaultValue write SetDefaultValue;
+  {$DEFINE UNS_NodeInclude_Declaration}
+    {$INCLUDE '.\UniSettings_Node.inc'}
+  {$UNDEF UNS_NodeInclude_Declaration}
   end;
 
 implementation
@@ -52,51 +29,36 @@ uses
   BinaryStreaming, FloatHex,
   UniSettings_Exceptions;
 
-procedure TUNSNodeTime.SetValue(NewValue: TTime);
-begin
-If Frac(NewValue) <> Frac(fValue) then
-  begin
-    fValue := Frac(NewValue);
-    DoChange;
-  end;
-end;
+type
+  TUNSNodeClassType = TUNSNodeTime;
 
-//------------------------------------------------------------------------------
+var
+  UNS_StreamWriteFunction:
+    Function(Stream: TStream; Value: Float64; Advance: Boolean = True): TMemSize
+      = BinaryStreaming.Stream_WriteFloat64;
 
-procedure TUNSNodeTime.SetDefaultValue(NewValue: TTime);
-begin
-If Frac(NewValue) <> Frac(fDefaultValue) then
-  begin
-    fDefaultValue := Frac(NewValue);
-    DoChange;
-  end;
-end;
+  UNS_StreamReadFunction:
+    Function(Stream: TStream; Advance: Boolean = True): Float64
+      = BinaryStreaming.Stream_ReadFloat64;
+
+  UNS_BufferWriteFunction:
+    Function(Dest: Pointer; Value: Float64): TMemSize
+      = BinaryStreaming.Ptr_WriteFloat64;
+      
+  UNS_BufferReadFunction:
+    Function(Dest: Pointer): Float64
+      = BinaryStreaming.Ptr_ReadFloat64;
 
 //==============================================================================
 
-class Function TUNSNodeTime.GetValueType: TUNSValueType;
+class Function TUNSNodeClassType.GetValueType: TUNSValueType;
 begin
 Result := vtTime;
 end;
 
 //------------------------------------------------------------------------------
 
-Function TUNSNodeTime.GetValueSize: TMemSize;
-begin
-Result := SizeOf(TTime);
-end;
-
-//------------------------------------------------------------------------------
-
-Function TUNSNodeTime.GetDefaultValueSize: TMemSize;
-begin
-Result := SizeOf(TTime);
-end;
-
-
-//------------------------------------------------------------------------------
-
-Function TUNSNodeTime.ConvToStr(Value: TTime): String;
+Function TUNSNodeClassType.ConvToStr(Value: TUNSNodeValueType): String;
 begin
 If ValueFormatSettings.HexDateTime then
   Result := '$' + DoubleToHex(Value)
@@ -106,7 +68,7 @@ end;
 
 //------------------------------------------------------------------------------
 
-Function TUNSNodeTime.ConvFromStr(const Str: String): TTime;
+Function TUNSNodeClassType.ConvFromStr(const Str: String): TUNSNodeValueType;
 begin
 If Length(Str) > 1 then
   begin
@@ -120,7 +82,7 @@ end;
 
 //==============================================================================
 
-constructor TUNSNodeTime.Create(const Name: String; ParentNode: TUNSNodeBase);
+constructor TUNSNodeClassType.Create(const Name: String; ParentNode: TUNSNodeBase);
 begin
 inherited Create(Name,ParentNode);
 fValue := Now;
@@ -130,149 +92,9 @@ end;
 
 //------------------------------------------------------------------------------
 
-constructor TUNSNodeTime.CreateAsCopy(Source: TUNSNodeBase; const Name: String; ParentNode: TUNSNodeBase);
-begin
-inherited CreateAsCopy(Source,Name,ParentNode);
-fValue := TUNSNodeTime(Source).Value;
-fSavedValue := TUNSNodeTime(Source).SavedValue;
-fDefaultValue := TUNSNodeTime(Source).DefaultValue;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeTime.ActualFromDefault;
-begin
-If not ActualEqualsDefault then
-  begin
-    fValue := fDefaultValue;
-    DoChange;
-  end;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeTime.DefaultFromActual;
-begin
-If not ActualEqualsDefault then
-  begin
-    fDefaultValue := fValue;
-    DoChange;
-  end;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeTime.ExchangeActualAndDefault;
-var
-  Temp: TTime;
-begin
-If not ActualEqualsDefault then
-  begin
-    Temp := fDefaultValue;
-    fDefaultValue := fValue;
-    fValue := Temp;
-    DoChange;
-  end;
-end;
-
-//------------------------------------------------------------------------------
-
-Function TUNSNodeTime.ActualEqualsDefault: Boolean;
-begin
-Result := Frac(fValue) = Frac(fDefaultValue);
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeTime.Save;
-begin
-fSavedValue := fValue;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeTime.Restore;
-begin
-SetValue(fSavedValue);
-end;
-
-//------------------------------------------------------------------------------
-
-Function TUNSNodeTime.Address(AccessDefVal: Boolean = False): Pointer;
-begin
-If AccessDefVal then
-  Result := Addr(fDefaultValue)
-else
-  Result := Addr(fValue);
-end;
-
-//------------------------------------------------------------------------------
-
-Function TUNSNodeTime.AsString(AccessDefVal: Boolean = False): String;
-begin
-If AccessDefVal then
-  Result := ConvToStr(fDefaultValue)
-else
-  Result := ConvToStr(fValue);
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeTime.FromString(const Str: String; AccessDefVal: Boolean = False);
-begin
-If AccessDefVal then
-  SetDefaultValue(ConvFromStr(Str))
-else
-  SetValue(ConvFromStr(Str));
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeTime.ToStream(Stream: TStream; AccessDefVal: Boolean = False);
-begin
-If AccessDefVal then
-  Stream_WriteFloat64(Stream,Frac(fDefaultValue))
-else
-  Stream_WriteFloat64(Stream,Frac(fValue));
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeTime.FromStream(Stream: TStream; AccessDefVal: Boolean = False);
-begin
-If AccessDefVal then
-  SetDefaultValue(Frac(Stream_ReadFloat64(Stream)))
-else
-  SetValue(Frac(Stream_ReadFloat64(Stream)));
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeTime.ToBuffer(Buffer: TMemoryBuffer; AccessDefVal: Boolean = False);
-begin
-If Buffer.Size >= ObtainValueSize(AccessDefVal) then
-  begin
-    If AccessDefVal then
-      Ptr_WriteFloat64(Buffer.Memory,Frac(fDefaultValue))
-    else
-      Ptr_WriteFloat64(Buffer.Memory,Frac(fValue));
-  end
-else raise EUNSBufferTooSmallException.Create(Buffer,Self,'GetValueToBuffer');
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TUNSNodeTime.FromBuffer(Buffer: TMemoryBuffer; AccessDefVal: Boolean = False);
-begin
-If Buffer.Size >= ObtainValueSize(AccessDefVal) then
-  begin
-    If AccessDefVal then
-      SetDefaultValue(Frac(Ptr_ReadFloat64(Buffer.Memory)))
-    else
-      SetValue(Frac(Ptr_ReadFloat64(Buffer.Memory)));
-  end
-else raise EUNSBufferTooSmallException.Create(Buffer,Self,'SetValueFromBuffer');
-end;
+{$DEFINE UNS_NodeInclude_Implementation}
+  {$INCLUDE '.\UniSettings_Node.inc'}
+{$UNDEF UNS_NodeInclude_Implementation}
 
 {$WARNINGS OFF} // supresses warnings on lines after the final end
 end.
